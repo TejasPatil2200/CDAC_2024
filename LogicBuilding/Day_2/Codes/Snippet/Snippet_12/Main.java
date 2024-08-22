98b782a2a687b43ef09b5b08ac9259889fa38dfa
Tejas Patil <55108295+TejasPatil2200@users.noreply.github.com>
2024-08-22 16:02:48 +0530

    Add files via upload

---
+++ LogicBuilding/Day_2/Codes/Snippet/Snippet_12/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+ public static void main(String[] args) {
+ if (true) {
+ System.out.println("Infinite Loop");
+ }
+else{
+System.out.println("Statement is a False");}
+ }
+} 
+
+/*
+Infinite Loop
+Infinite Loop
+Infinite Loop
+Infinite Loop
+Infinite Loop
+*/
+
+Ans: Change the while loop to if-else condition.
