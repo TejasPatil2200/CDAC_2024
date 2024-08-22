62032c5bce6d8097b388de95d7fec52875210e0d
Tejas Patil <55108295+TejasPatil2200@users.noreply.github.com>
2024-08-22 15:59:49 +0530

    Add files via upload

---
+++ LogicBuilding/Day_2/Codes/Snippet/Snippet_7/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+ public static void main(String[] args) {
+ String x = "Hello";
+ System.out.println(x);
+ }
+} 
+
+/*
+Main.java:3: error: incompatible types: String cannot be converted to int
+ int x = "Hello";
+         ^
+1 error
+
+*/
