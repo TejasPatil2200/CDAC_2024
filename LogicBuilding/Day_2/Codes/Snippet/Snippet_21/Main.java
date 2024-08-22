68fa1a3a35c8bb637f8b61233d47e60e60e9b4aa
Tejas Patil <55108295+TejasPatil2200@users.noreply.github.com>
2024-08-22 16:14:38 +0530

    Add files via upload

---
+++ LogicBuilding/Day_2/Codes/Snippet/Snippet_21/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+ public static void main(String[] args) {
+ System.out.println("Hello, World!");
+ }
+} 
+
+/*
+Main.java:5: error: reached end of file while parsing
+}
+ ^
+1 error
+
+Ans: Mismatched brackets will allow the compiler to fail the compilation of the code.
+
+*/
