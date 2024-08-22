b20a679ae6d08625ba9118650f821e5d0e0faaac
Tejas Patil <55108295+TejasPatil2200@users.noreply.github.com>
2024-08-22 16:14:16 +0530

    Add files via upload

---
+++ LogicBuilding/Day_2/Codes/Snippet/Snippet_20/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+ public static void main(String[] args) {
+ System.out.println("Hello, World");
+ }
+} 
+
+
+/*
+Main.java:3: error: ';' expected
+ System.out.println("Hello, World")
+                                   ^
+1 error
+
+Ans: Many programming languages like Java and C require proper use of semicolons as they represent a pause in a line of code.
+
+
+*/
