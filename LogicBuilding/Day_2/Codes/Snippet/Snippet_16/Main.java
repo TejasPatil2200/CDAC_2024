d5570f0a6f71e8b59c18aab84c44427d159291a3
Tejas Patil <55108295+TejasPatil2200@users.noreply.github.com>
2024-08-22 16:12:35 +0530

    Add files via upload

---
+++ LogicBuilding/Day_2/Codes/Snippet/Snippet_16/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+ public static void main(String[] args) {
+ int num = 10;
+ int result = num / 4;
+ System.out.println(result);
+ }
+} 
+
+
+/*
+In a division operation the output is not in decimal format, So changing the datatype of result variable to int will allow us to get a 
+non decimal value which is sutiable for the devision operation.
+
+*/
