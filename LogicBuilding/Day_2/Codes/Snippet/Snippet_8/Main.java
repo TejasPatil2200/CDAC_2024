79574095130a7ccc55daff500d3ea3a4d05a7a00
Tejas Patil <55108295+TejasPatil2200@users.noreply.github.com>
2024-08-22 16:00:13 +0530

    Add files via upload

---
+++ LogicBuilding/Day_2/Codes/Snippet/Snippet_8/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+ public static void main(String[] args) {
+ System.out.println("Hello, World!");
+ }
+} 
+
+/*
+Main.java:3: error: ')' expected
+ System.out.println("Hello, World!"
+                                   ^
+1 error
+*/
