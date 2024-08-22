43f11cf0dae1ec432a32bbc56428b437e68617aa
Tejas Patil <55108295+TejasPatil2200@users.noreply.github.com>
2024-08-22 15:59:04 +0530

    Add files via upload

---
+++ LogicBuilding/Day_2/Codes/Snippet/Snippet_6/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+ public static void main(String[] args) {
+ int y = 5;
+ int x = y + 10;
+ System.out.println(x);
+ }
+} 
+
+
+/*
+Main.java:3: error: cannot find symbol
+ int x = y + 10;
+         ^
+  symbol:   variable y
+  location: class Main
+1 error
+*/
