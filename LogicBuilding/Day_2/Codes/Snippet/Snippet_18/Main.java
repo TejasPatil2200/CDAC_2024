8fa557e2f7cf31f844f8ad852438639d647c2223
Tejas Patil <55108295+TejasPatil2200@users.noreply.github.com>
2024-08-22 16:13:26 +0530

    Add files via upload

---
+++ LogicBuilding/Day_2/Codes/Snippet/Snippet_18/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+ public static void main(String[] args) {
+ int a = 10;
+ int b = 5;
+ int result = a + b * 2;
+ System.out.println(result);
+ }
+} 
+
+/*
+Ans: In the above code the language follow the mathematical standards
+to perform the mathematical operations AKA BODMAS. So in the above code first b*2 is done as 10 and then 10+10 which results as 20.
