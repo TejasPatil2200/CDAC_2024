52bfafa1d8f3a4c253cb5155c2a99d9f2f8c0386
Tejas Patil <55108295+TejasPatil2200@users.noreply.github.com>
2024-08-22 15:53:27 +0530

    Add files via upload

---
+++ LogicBuilding/Day_2/Codes/Snippet/Snippet_3/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+ public static void main(String[] args) {
+ System.out.println("Hello, World!");
+ return;
+ }
+}
+
+/*
+
+Error: Main method must return a value of type void in class Main, please
+define the main method as:
+   public static void main(String[] args)
+
+
+Main.java:4: error: incompatible types: unexpected return value
+ return 0;
+
+*/
