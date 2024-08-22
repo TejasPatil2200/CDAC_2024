a0c8830b654fced26237ab69b4132b165eadd6a9
Tejas Patil <55108295+TejasPatil2200@users.noreply.github.com>
2024-08-22 16:00:41 +0530

    Add files via upload

---
+++ LogicBuilding/Day_2/Codes/Snippet/Snippet_9/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+ public static void main(String[] args) {
+ int c = 10;
+ System.out.println(c);
+ }
+} 
+
+
+/*
+Main.java:3: error: not a statement
+ int class = 10;
+ ^
+Main.java:3: error: ';' expected
+ int class = 10;
+    ^
+Main.java:3: error: <identifier> expected
+ int class = 10;
+          ^
+Main.java:4: error: illegal start of expression
+ System.out.println(class);
+                    ^
+Main.java:4: error: <identifier> expected
+ System.out.println(class);
+                         ^
+5 errors
+*/
